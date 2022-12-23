e4f5f952829e6f6e259b171916efecc13f0d0705
Hyojin <88768022+su-no@users.noreply.github.com>
2022-12-24 00:51:18 +0900

    Feat: 상세페이지 상품설명, 레시피 이미지 동적 fetch

--- src/components/DescriptionImage/DescriptionImage.jsx
+++ src/components/DescriptionImage/DescriptionImage.jsx
@@ -1,22 +1,25 @@
-import React from 'react';
-// import { useParams } from 'react-router-dom';
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import axios from 'axios';
 
-// export default function Description({ imgPath }) {
-//   return (
-//     <img
-//       src={process.env.PUBLIC_URL + imgPath}
-//       style={{ width: '100%' }}
-//       alt="img"
-//     />
-//   );
-// }
 export default function Description() {
-  // const { id } = useParams();
-  // console.log(id);
+  const { id } = useParams();
+  const [imgPath, setImgPath] = useState('');
+
+  // db.json에서 상품설명 이미지 경로 받아오는 함수
+  const getImgPath = async () => {
+    const response = await axios.get(`http://localhost:3001/kimchis/${id}`);
+    const data = response.data['detail-image'];
+    setImgPath(data);
+  };
+
+  useEffect(() => {
+    getImgPath();
+  });
 
   return (
     <img
-      src={process.env.PUBLIC_URL + '/images/product/kimchi_1-detail.png'}
+      src={process.env.PUBLIC_URL + imgPath}
       style={{ padding: '10px', width: '100%', boxSizing: 'border-box' }}
       alt="img"
     />
