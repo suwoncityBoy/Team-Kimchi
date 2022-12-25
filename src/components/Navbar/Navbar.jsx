734d051dc199fde90cf99a22c49ddf28e0332957
Yoo Seungmin <dnjfht@naver.com>
2022-12-25 16:38:52 +0900

    navbar Link 제거

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,6 +1,5 @@
 import { React, useState } from 'react';
 import { BsCart3 } from 'react-icons/bs';
-import { Link } from 'react-router-dom';
 import { StyledContainer, StyledWrap } from './Navbar.style';
 
 export default function Navbar() {
