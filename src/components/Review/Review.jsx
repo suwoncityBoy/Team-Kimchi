b50199ac99b6e386e9a72f60f9f135f15f49f1c6
Hyojin <88768022+su-no@users.noreply.github.com>
2022-12-26 18:58:18 +0900

    Feat: 상세페이지 - 댓글 조회, 수정, 삭제 기능 구현

--- src/components/Review/Review.jsx
+++ src/components/Review/Review.jsx
@@ -1,32 +1,200 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useState } from 'react';
+import { checkPassword } from '../../utils/validate';
 
-export default function Review({ nickname, content, date }) {
+export default function Review({
+  id,
+  kimchiId,
+  nickname,
+  content,
+  date,
+  setReviewData,
+}) {
+  const [hover, setHover] = useState(false);
+  const [isTextareaVisible, setIsTextareaVisible] = useState(false);
+  const [editContent, setEditContent] = useState(content);
+
+  const checkDBPassword = async (input) => {
+    // 비밀번호 유효성 검사
+    if (!checkPassword(input)) return false;
+    // 서버에 비밀번호 맞는지 요청
+    const response = await axios.get(`http://localhost:3001/reviews?id=${id}`);
+    const password = response.data[0].password;
+    return password === input ? true : false;
+  };
+
+  // 수정 버튼 클릭
+  const handleEditBtnClick = async () => {
+    const inputPassword = prompt('비밀번호를 입력하세요.');
+    const checked = await checkDBPassword(inputPassword);
+    if (!checked) {
+      console.log('틀린 비밀번호 입니다.');
+      return;
+    }
+    // 수정할 textarea 보여주기
+    setIsTextareaVisible(true);
+  };
+
+  // 수정 폼 제출
+  const handleEditSubmit = async (e) => {
+    e.preventDefault();
+
+    // 서버에 데이터 PATCH
+    await axios.patch(`http://localhost:3001/reviews/${id}`, {
+      content: editContent,
+    });
+    alert('수정이 완료되었습니다.');
+
+    // 데이터 다시 GET, state 업데이트
+    const response = await axios.get(
+      `http://localhost:3001/reviews?kimchiId=${kimchiId}`,
+    );
+    setReviewData(response.data);
+
+    // textarea 숨기기
+    setIsTextareaVisible(false);
+  };
+
+  // 삭제 버튼 클릭
+  const handleDeleteBtnClick = async () => {
+    const inputPassword = prompt('비밀번호를 입력하세요.');
+    const checked = checkPassword(inputPassword);
+    if (!checked) {
+      console.log('틀린 비밀번호 입니다.');
+      return;
+    }
+
+    if (window.confirm('정말 삭제하시겠습니까??')) {
+      // 서버에 데이터 DELETE
+      await axios.delete(`http://localhost:3001/reviews/${id}`);
+      // 데이터 다시 GET, state 업데이트
+      const response = await axios.get(
+        `http://localhost:3001/reviews?kimchiId=${kimchiId}`,
+      );
+      setReviewData(response.data);
+    }
+  };
+
   return (
-    <li style={reviewStyle}>
+    <li
+      style={reviewStyle}
+      onMouseEnter={() => setHover(true)}
+      onMouseLeave={() => setHover(false)}
+    >
       <div style={nicknameStyle}>{nickname}</div>
-      <div style={contentStyle}>{content}</div>
-      <div style={dateStyle}>{date}</div>
+      {isTextareaVisible ? (
+        // 수정 상태에서는 form 보여주기
+        <form
+          onSubmit={handleEditSubmit}
+          style={{
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'flex-end',
+          }}
+        >
+          <textarea
+            style={textareaStyle}
+            value={editContent}
+            onChange={(e) => setEditContent(e.target.value)}
+            autoFocus={true}
+          ></textarea>
+          <button style={btnStyle}>확인</button>
+        </form>
+      ) : (
+        // 수정 상태가 아닐 때는 내용 보여주기
+        <div style={contentStyle}>{content}</div>
+      )}
+      <div style={rightStyle}>
+        <div style={dateStyle}>{date}</div>
+        <div>
+          <button
+            style={
+              // 리뷰 hover 상태일 때 버튼 보여주기
+              hover
+                ? { ...hoverBtnStyle, visibility: 'visible' }
+                : hoverBtnStyle
+            }
+            onClick={handleEditBtnClick}
+          >
+            수정
+          </button>
+          <button
+            style={
+              hover
+                ? { ...hoverBtnStyle, visibility: 'visible' }
+                : hoverBtnStyle
+            }
+            onClick={handleDeleteBtnClick}
+          >
+            삭제
+          </button>
+        </div>
+      </div>
     </li>
   );
 }
 
 const reviewStyle = {
   display: 'flex',
+  flexDirection: 'row',
   justifyContent: 'space-between',
-  margin: '16px 0 20px 0',
+  alignItems: 'start',
+  margin: '16px 0',
+  lineHeight: '1.5',
+  paddingBottom: '16px',
+  borderBottom: '1px solid #cdcdcd',
 };
 
 const nicknameStyle = {
-  background: 'pink',
   width: '100px',
+  fontWeight: '500',
 };
 
 const contentStyle = {
-  background: 'skyblue',
-  flex: '1 1 auto',
+  width: '600px',
+  textAlign: 'left',
+};
+
+const textareaStyle = {
+  width: '600px',
+  height: '100px',
+  resize: 'none',
+  border: '1px solid #c2c2c2',
+  borderRadius: '4px',
+  fontSize: '16px',
+  fontFamily: 'normal',
 };
 
 const dateStyle = {
-  background: 'lavender',
-  width: '200px',
+  width: '150px',
+  color: '#828282',
+};
+
+const rightStyle = {
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'center',
+};
+
+const btnStyle = {
+  fontSize: '14px',
+  border: '1px solid grey',
+  borderRadius: '4px',
+  background: 'transparent',
+  padding: '4px 8px ',
+  margin: '10px 0',
+  cursor: 'pointer',
+  width: '50px',
+};
+
+const hoverBtnStyle = {
+  fontSize: '14px',
+  border: '1px solid grey',
+  borderRadius: '4px',
+  background: 'transparent',
+  padding: '4px 8px ',
+  margin: '10px 5px',
+  visibility: 'hidden',
+  cursor: 'pointer',
+  width: '50px',
 };
--- src/components/ReviewContainer/ReviewContainer.jsx
+++ src/components/ReviewContainer/ReviewContainer.jsx
@@ -2,38 +2,61 @@ import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import axios from 'axios';
 import getDate from '../../utils/getDate';
-import { validateNickname, validatePassword } from '../../utils/validate';
+import {
+  checkContent,
+  checkNickname,
+  checkPassword,
+} from '../../utils/validate';
 import Review from '../Review/Review';
 
 export default function ReviewContainer() {
   const { id: kimchiId } = useParams();
+
+  // 서버에서 불러오는 review 데이터
   const [reviewData, setReviewData] = useState([]);
+
+  // input 값
   const [nickname, setNickname] = useState('');
   const [password, setPassword] = useState('');
   const [content, setContent] = useState('');
 
-  const handleSubmit = (e) => {
+  // 경고 문구
+  const [alertText, setAlertText] = useState('');
+  const [alertDisplay, setAlertDisplay] = useState(false);
+
+  const handleSubmit = async (e) => {
     e.preventDefault();
 
-    // 유효성 검사
-    const isValidNickname = validateNickname(nickname);
+    // 닉네임 유효성 검사
+    setNickname((name) => name.trim());
+    const isValidNickname = checkNickname(nickname);
     if (!isValidNickname) {
-      console.log(
+      setAlertText(
         '닉네임은 한글, 영어, 숫자로 이루어진 2자 이상 10자 이하의 문자열입니다.',
       );
-      // TODO: 경고표시 후 리턴
+      setAlertDisplay(true);
       return;
     }
 
-    const isValidPassword = validatePassword(password);
+    // 비밀번호 유효성 검사
+    const isValidPassword = checkPassword(password);
     if (!isValidPassword) {
-      console.log('비밀번호는 숫자 4자리입니다.');
-      // TODO: 초기화 후 리턴
+      setAlertText('비밀번호는 숫자 4자리입니다.');
+      setAlertDisplay(true);
+      setPassword('');
       return;
     }
 
-    // TODO: 내용 유효성 검사
+    // 내용 유효성 검사
+    setContent((content) => content.trim());
+    const isValidContent = checkContent(content);
+    if (!isValidContent) {
+      setAlertText('내용은 10자 이상 300자 이하입니다.');
+      setAlertDisplay(true);
+      return;
+    }
 
+    // 서버에 데이터 POST
     const data = {
       kimchiId,
       nickname,
@@ -41,9 +64,22 @@ export default function ReviewContainer() {
       content,
       date: getDate(),
     };
+    await axios.post('http://localhost:3001/reviews', data);
 
-    axios.post('http://localhost:3001/reviews', data);
-    console.log('review 저장 성공');
+    // review 데이터 GET -> state 업데이트
+    const response = await axios.get(
+      `http://localhost:3001/reviews?kimchiId=${kimchiId}`,
+    );
+    setReviewData(response.data);
+
+    // 입력 폼 초기화
+    setNickname('');
+    setPassword('');
+    setContent('');
+
+    // 경고 문구 초기화
+    setAlertDisplay(false);
+    setAlertText('');
   };
 
   useEffect(() => {
@@ -53,17 +89,9 @@ export default function ReviewContainer() {
   }, [kimchiId]);
 
   return (
-    <div
-      style={{
-        maxWidth: '1010px',
-        margin: 'auto',
-        display: 'flex',
-        padding: '10px',
-        flexDirection: 'column',
-      }}
-    >
+    <div style={containerStyle}>
       {/* 리뷰 입력 폼 */}
-      <form onSubmit={handleSubmit} style={{ width: '100%' }}>
+      <form onSubmit={handleSubmit} style={formStyle}>
         <div style={{ textAlign: 'left', marginBottom: '30px' }}>
           <label>
             닉네임
@@ -73,9 +101,7 @@ export default function ReviewContainer() {
               onChange={(e) => {
                 setNickname(e.target.value);
               }}
-              required
-              minLength="1"
-              maxLength="10"
+              maxLength="7"
               style={inputStyle}
             />
           </label>
@@ -87,20 +113,14 @@ export default function ReviewContainer() {
               onChange={(e) => {
                 setPassword(e.target.value);
               }}
-              required
-              minLength="4"
               maxLength="4"
               style={inputStyle}
             />
           </label>
+          {/* 경고 문구 */}
+          {alertDisplay && <p style={alertStyle}>{alertText}</p>}
         </div>
-        <label
-          style={{
-            display: 'flex',
-            flexDirection: 'column',
-            textAlign: 'left',
-          }}
-        >
+        <label style={contentLabelStyle}>
           자세한 후기를 들려주세요
           <textarea
             type="text"
@@ -108,7 +128,7 @@ export default function ReviewContainer() {
             onChange={(e) => {
               setContent(e.target.value);
             }}
-            required
+            // required
             minLength="10"
             maxLength="300"
             style={textareaStyle}
@@ -131,9 +151,12 @@ export default function ReviewContainer() {
           {reviewData.map((review) => (
             <Review
               key={review.id}
+              id={review.id}
+              kimchiId={kimchiId}
               nickname={review.nickname}
               content={review.content}
               date={review.date}
+              setReviewData={setReviewData}
             />
           ))}
         </ul>
@@ -142,19 +165,17 @@ export default function ReviewContainer() {
   );
 }
 
-const textareaStyle = {
-  resize: 'none',
-  margin: '16px 0',
-  height: '200px',
-  border: '1px solid #c2c2c2',
-  borderRadius: '4px',
+const containerStyle = {
+  maxWidth: '1010px',
+  margin: 'auto',
+  display: 'flex',
+  padding: '10px',
+  flexDirection: 'column',
 };
 
-const btnStyle = {
-  backgroundColor: 'red',
-  padding: '10px 20px',
-  border: 'none',
-  color: ' white',
+const formStyle = {
+  width: '100%',
+  marginBottom: '20px',
 };
 
 const inputStyle = {
@@ -163,4 +184,36 @@ const inputStyle = {
   width: '200px',
   border: '1px solid #c2c2c2',
   borderRadius: '4px',
+  fontFamily: 'normal',
+  fontSize: '16px',
+};
+
+const contentLabelStyle = {
+  display: 'flex',
+  flexDirection: 'column',
+  textAlign: 'left',
+};
+
+const textareaStyle = {
+  fontSize: '16px',
+  resize: 'none',
+  margin: '16px 0',
+  height: '200px',
+  border: '1px solid #c2c2c2',
+  borderRadius: '4px',
+  fontFamily: 'normal',
+};
+
+const alertStyle = {
+  marginTop: '10px',
+  color: 'red',
+};
+
+const btnStyle = {
+  backgroundColor: '#d82424',
+  padding: '10px 20px',
+  border: 'none',
+  borderRadius: '4px',
+  color: ' white',
+  cursor: 'pointer',
 };
