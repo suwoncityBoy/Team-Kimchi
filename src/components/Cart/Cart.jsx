958010c59016df3bc9a3acf034924bac9dcc8023
freesian <zzte123@naver.com>
2022-12-27 17:50:20 +0900

    221227_장바구니 수정!

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -6,16 +6,15 @@ import { changeChecked } from '../../redux/modules/cartSlice';
 
 // 데이터 카테고리별 컴포넌트
 export function DataList({
-  datas,
-  checkHandler,
-  checkedState,
-  setAllPay,
-  allPay,
-  removeMonoCheck,
-  setStock,
-  stock,
+  datas, // 장바구니 데이터들
+  checkHandler, //체크박스
+  checkedState, //체크박스
+  setAllPay, // 총 금액
+  allPay, // 총 금액
+  removeMonoCheck, // 데이터 삭제
+  setStock, // 갯수
+  stock, // 갯수
 }) {
-  console.log(stock);
   if (datas.length) {
     return (
       <>
@@ -27,7 +26,7 @@ export function DataList({
           return (
             <div>
               <Data
-                data={kimchi}
+                data={kimchi} // 데이터
                 checkHandler={(index) => checkHandler(index)}
                 index={index}
                 checkedState={checkedState}
@@ -47,71 +46,17 @@ export function DataList({
 
 // 데이터 카테고리에 들어가는 데이터
 function Data({
-  data,
-  checkHandler,
-  index,
-  checkedState,
-  setAllPay,
-  allPay,
-  removeMonoCheck,
-  setStock,
-  count,
+  data, // 김치 데이터
+  index, // 장바구니 리스트 인덱스
+  checkHandler, // 체크박스 핸들러
+  checkedState, // 체크박스 state
+  setAllPay, // 전체 금액
+  allPay, // 전체 금액
+  removeMonoCheck, // 데이터 삭제 버튼
+  setStock, // 갯수
+  count, // 갯수
 }) {
-  const dispatch = useDispatch();
-
-  // const { inCart } = useSelector((state) => state.user); // 장바구니 데이터
-
-  // const kimchi = [...inCart].filter(
-  //   (kimchis) => kimchis.id === data.id && kimchis.category === data.category,
-  // )[0];
-
-  // const checkedItemHandler = (id, isChecked) => {
-  //   if (isChecked) {
-  //     checkedItems.add(id);
-  //     setCheckedItems(checkedItems);
-  //   } else if (!isChecked && checkedItems.has(id)) {
-  //     checkedItems.delete(id);
-  //     setCheckedItems(checkedItems);
-  //   }
-  // };
-
-  // const allCheckHandler = () => setChecked(isAllChecked);
-
-  // useEffect(() => allCheckHandler(), [isAllChecked]);
-
-  // const [bChecked, setBChecked] = useState(false);
-
-  // const checkHandler = ({ target }) => {
-  //   setBChecked(!bChecked);
-  //   pCheckHandler(data.id, target.checked);
-  // };
-
-  // const allCheckHandler = () => setBChecked(isAllChecked);
-
-  // useEffect(() => allCheckHandler(), [isAllChecked]);
-  // const theKimchi = { ...data };
-  // theKimchi.checked = !theKimchi.checked;
-
-  // 카트 페이지랑 겹침 나중에 합치기
-  // const checkHandler = () => {
-  //   // 체크 박스를 누른 경우
-  //   const arrData = [...inCart];
-  //   for (let i in arrData) {
-  //     if (arrData[i].id === kimchi.id) {
-  //       const data = { ...arrData[i] };
-  //       data.checked = !kimchi.checked;
-  //       arrData[i] = data;
-  //     }
-  //   }
-
-  //   dispatch(changeChecked(arrData));
-  //   dispatch(
-  //     !kimchi.checked
-  //       ? plusPrice(kimchi.price * stock)
-  //       : minusPrice(kimchi.price * stock),
-  //   );
-  //   //setIsChecked(!isChecked);
-  // };
+  //const dispatch = useDispatch();
 
   const pop = () => {
     removeMonoCheck(index);
@@ -186,74 +131,3 @@ function Data({
     </div>
   );
 }
-
-function CheckBox({ data }) {
-  const { inCart } = useSelector((state) => state.user); // 장바구니 데이터
-
-  const kimchi = [...inCart].filter(
-    (kimchis) => kimchis.id === data.id && kimchis.category === data.category,
-  )[0];
-
-  const dispatch = useDispatch();
-
-  // const theKimchi = { ...data };
-  // theKimchi.checked = !theKimchi.checked;
-
-  // 카트 페이지랑 겹침 나중에 합치기
-  const checkHandler = () => {
-    const arrData = [...inCart];
-    for (let i in arrData) {
-      if (arrData[i].id === kimchi.id) {
-        const data = { ...arrData[i] };
-        data.checked = !kimchi.checked;
-        arrData[i] = data;
-      }
-    }
-
-    dispatch(changeChecked(arrData));
-    dispatch(
-      !kimchi.checked ? plusPrice(kimchi.price) : minusPrice(kimchi.price),
-    );
-    //setIsChecked(!isChecked);
-  };
-
-  return (
-    <label style={{ width: '24px', height: '24px', marginRight: '2%' }}>
-      <input
-        type="checkbox"
-        checked={kimchi.checked}
-        key={data.id}
-        onChange={checkHandler}
-      />
-    </label>
-  );
-}
-
-// export function CheckBoxList({datas}) {
-
-//   const [isCheckAll, setIsCheckAll] = useState(false); // 전체체크 상태 관리
-//   const [isChecking, setIsChecking] = useState(false); // 체크한 항목이 하나라도 있을경우를 관리
-//   const [arrChecked, setArrChecked] = useState(datas); // 체크된 항목을 관리
-
-//   const changeAllCheck = (event) => {
-//     if (event.target.checked) {
-//       setIsCheckAll(true); //전체 체크
-//     } else {
-//       setIsChecking(false);
-//       setArrChecked([]);
-//     }
-//     const checkingCheckedBos = () => {
-//       setIsCheckAll(false);
-//       setIsChecking(true);
-//       setArrChecked([]);
-//     };
-//   };
-//   return (
-//     <input
-//       type="checkbox"
-//       style={choiceRightMargin10}
-//       //onClick={pop}
-//       checked
-//     />
-//   );
-// }
--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -8,81 +8,36 @@ import { popItem, clearItem } from '../redux/modules/cartSlice';
 //import { CiCircleCheck } from 'react-icons/ci';
 
 export default function Cart() {
-  // const navigate = useNavigate();
-  // navigate(``)
-  const { inCart, totalSum } = useSelector((state) => state.user); // 장바구니 데이터
-  // const { lastSum } = useSelector((state) => state.user); // 장바구니 데이터
-  const [sum, setSum] = useState(totalSum);
-  const [checked, setChecked] = useState(true);
+  // 장바구니 데이터
+  const { inCart } = useSelector((state) => state.user);
 
-  //const [lastSum, setLastSum] = useState(totalSum);
-  const kindCabbage = inCart.filter(
-    (kimchis) => kimchis.category === '배추과김치',
-  ); // 배추과김치 데이터
-  const kindRadish = inCart.filter(
-    (kimchis) => kimchis.category === '무과김치',
-  ); // 무과김치 데이터
-  const kindRoot = inCart.filter(
-    (kimchis) => kimchis.category === '뿌리과김치',
-  ); // 무과김치 데이터
-  const kindEtc = inCart.filter((kimchis) => kimchis.category === '기타김치'); // 무과김치 데이터
-
-  // const [isAllChecked, setIsAllChecked] = useState(
-  //   inCart.filter((kimchis) => kimchis.checked === true).length ===
-  //     inCart.length
-  //     ? true
-  //     : false,
-  // );
-
-  const issues = [...inCart]; // [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-
-  // const [isAllChecked, setIsAllChecked] = useState(true);
-  // const [checkedItems, setCheckedItems] = useState(issues);
-  // const [checkedItem, setCheckedItem] = useState(new Set());
-
-  // const allCheckedHandler = (event) => {
-  //   if (event.target.checked) {
-  //     setCheckedItems(issues.map((item) => item));
-  //     setIsAllChecked(true);
-  //   } else {
-  //     setCheckedItems(checkedItems ?? []);
-  //     //setCheckedItems(setCheckedItems);
-  //     setIsAllChecked(false);
-  //   }
-  // };
-
-  // const checkedItemHandler = (id, isChecked) => {
-  //   if (isChecked) {
-  //     checkedItem.add(id);
-  //     setCheckedItems(checkedItem);
-  //   } else if (
-  //     !isChecked &&
-  //     checkedItems.filter((item) => item.id === id).length !== 0
-  //   ) {
-  //     setCheckedItems(checkedItems.filter((item) => item.id !== id));
-  //     setIsAllChecked(false);
-  //   }
-  // };
-
+  // 총 금액
   const [allPay, setAllPay] = useState(
     inCart.reduce((sum, kimchi) => {
       return sum + kimchi.price;
     }, 0),
   );
-  const [thisPay, setThisPay] = useState(
-    new Array(inCart.length).fill([{ ...inCart.price }]),
-  );
 
+  // 선택 제품 갯수
   const [stock, setStock] = useState(new Array(inCart.length).fill(1));
 
+  // 전체 선택 체크박스
   const [isAllChecked, setAllChecked] = useState(true);
+
+  // 체크박스 하나
   const [checkedState, setCheckedState] = useState(
     new Array(inCart.length).fill({ checked: true, active: 'block', price: 0 }),
   );
-  useEffect(() => {
-    console.log(stock);
-  }, [stock]);
 
+  // const [thisPay, setThisPay] = useState(
+  //   new Array(inCart.length).fill([{ ...inCart.price }]),
+  // );
+
+  /* 사용하는 훅 */
+  const dispatch = useDispatch();
+
+  /* 내가 만든  */
+  // 체크박스 모두 선택
   const handleAllCheck = (event) => {
     setAllChecked((prev) => !prev);
 
@@ -107,28 +62,16 @@ export default function Cart() {
       //return sum + kimchi.price;
     }, 0);
     event.target.checked === true ? setAllPay(sum) : setAllPay(0);
-    // isAllChecked === true
-    //   ? setAllPay(
-    //       inCart.reduce((sum, kimchi) => {
-    //         return sum + kimchi.price;
-    //       }, 0),
-    //     )
-    //   : setAllPay(0);
   };
 
+  // 전체 삭제 버튼
   const clearAll = () => {
     dispatch(clearItem());
     setCheckedState([{}]);
   };
 
+  // 조그마한 x(삭제) 버튼
   const removeMonoCheck = (number) => {
-    //const checkedStateClone = [...checkedState];
-    // for (let i in checkedStateClone) {
-    //   if (i * 1 === number) {
-    //     checkedStateClone[number].active =
-    //       checkedStateClone[number].active === 'block' ? 'none' : 'block';
-    //   }
-    // }
     const checkedStateClone = checkedState.reduce((datas, items, index) => {
       if (index === number) {
         datas.push({
@@ -153,6 +96,8 @@ export default function Cart() {
       clearAll();
     }
   };
+
+  // 체크박스
   const handleMonoCheck = (number) => {
     //number : index 번호
     const updatedCheckedState = checkedState.map((item, index) =>
@@ -176,73 +121,7 @@ export default function Cart() {
         ? allPay + inCart[number].price * stock[number] // 체크 된 경우
         : allPay - inCart[number].price * stock[number], // 체크 푼 경우
     );
-
-    // const datas = [...inCart];
-    // for (let i in datas) {
-    //   if (i * 1 === number) {
-    //     datas[i].checked = event.target.checked;
-    //   }
-    // }
-    // setAllPay(allPay - inCart[number].price);
-    //console.log(inCart[number].price);
   };
-
-  const dispatch = useDispatch();
-
-  // 처음부터 모든 체크박스에 체크가 되어있는 상황임.
-  // 체크를 하나라도 풀었을때 -> 전체체크박에 체크된거 풀기 :
-  // => 메소드를 만들어야할듯, isChecked 상태가 false 로 바뀌면 isAllChecked 도 false로 바뀌어야함
-  // let sums = 0;
-  // const allCheckHandler = () => {
-  //   const arrData = [...inCart];
-  //   let isCheck;
-  //   for (let i in arrData) {
-  //     const data = { ...arrData[i] };
-  //     data.checked = !isAllChecked;
-  //     isCheck = !isAllChecked;
-
-  //     arrData[i] = data;
-  //   }
-  //   setSum(isCheck === true ? totalSum : 0);
-  //   dispatch(changeChecked(arrData));
-  //   setIsAllChecked(!isAllChecked);
-  // };
-
-  // 모두다 체크 되었을때 -> 전체체크박스에 체크 하기
-  //
-
-  // 체크박스
-  // const [checkedItem, setCheckedItem] = useState(inCart); // 전체 체크박스 관리
-  // const [isAllChecked, setIsAllChecked] = useState(true); // 전체 선택 관리
-
-  // const checkHandler = (data, isChecked) => {
-  //   // 개별 체크 박스 관리 메소드
-  //   if (isChecked) {
-  //     setCheckedItem([...checkedItem, data]);
-
-  //     if (checkedItem.length === inCart.length) {
-  //       setIsAllChecked(true);
-  //     }
-  //   } else {
-  //     //오모 이렇게두 되네,,
-  //     setCheckedItem([...checkedItem.filter((item) => item.id !== data.id)]);
-  //     setIsAllChecked(false);
-  //   }
-  // };
-
-  // const allCheck = () => {
-  //   // 전체 체크 박스 관리 메소드
-  //   setIsAllChecked(true);
-  // };
-
-  // 선택삭제
-
-  // 수량 갯수
-
-  useEffect(() => {
-    // 페이지 이동 시 최상단으로 스크롤
-    window.scrollTo(0, 0);
-  }, []);
 
   return (
     <div style={thisPage}>
@@ -270,60 +149,57 @@ export default function Cart() {
       </div>
       {/* 장바구니 내용 영역*/}
       <div style={items}>
-        {/* <div>
-          <img src={`${process.env.PUBLIC_URL}/images/cabbage.png`}></img>
-        </div> */}
-        <ul style={item}>
-          <div style={{ display: 'flex', alignItems: 'center' }}>
-            <img src={`${process.env.PUBLIC_URL}/images/cabbage.png`}></img>
-            <span>배추과 김치</span>
-          </div>
-          <Data />
-          <Data />
-          <Data />
-        </ul>
-        <ul style={item}>
-          <div style={{ display: 'flex', alignItems: 'center' }}>
-            <img src={`${process.env.PUBLIC_URL}/images/cabbage.png`}></img>
-            <span>뿌리과 김치</span>
-          </div>
-          <Data />
-          <Data />
-          <Data />
-        </ul>
-      </div>
-      {/* 결제 금액 , 결제 버튼 영역*/}
-      <div
-        style={{
-          //backgroundColor: 'orange',
-          width: '100%',
-          height: 'auto',
-          display: 'grid',
-          justifyContent: 'center',
-        }}
-      >
-        <div
-          style={{
-            display: 'flex',
-            textAlign: 'center',
-            alignItems: 'center',
-            margin: '6rem 0 4rem 0',
-          }}
-        >
-          <div>결제 예정 금액 :</div>
-          <div>3000만원</div>
-        </div>
-        <input
-          type="button"
-          value="결제하기"
-          style={{
-            display: 'block',
-            // textAlign: 'center',
-            // alignItems: 'center',
-            height: '50px',
-            value: '1000',
-          }}
-        />
+        {checkedState.filter((item) => item.active === 'block').length !== 0 ? ( // 장바구니 데이터가 있을 경우에만
+          <>
+            <ul style={item}>
+              <DataList
+                datas={inCart}
+                checkHandler={(number) => handleMonoCheck(number)}
+                checkedState={checkedState}
+                setAllPay={setAllPay}
+                allPay={allPay}
+                removeMonoCheck={(number) => removeMonoCheck(number)}
+                setStock={setStock}
+                stock={stock}
+              />
+            </ul>
+
+            {/* 결제 금액 , 결제 버튼 영역*/}
+            <div
+              style={{
+                //backgroundColor: 'orange',
+                width: '100%',
+                height: '25%',
+                display: 'grid',
+                justifyContent: 'center',
+              }}
+            >
+              <div
+                style={{
+                  display: 'flex',
+                  textAlign: 'center',
+                  alignItems: 'center',
+                }}
+              >
+                <p></p>
+                <div>결정 예정 금액 : {allPay} 원</div>
+              </div>
+              <input
+                type="button"
+                value="결제하기"
+                style={{
+                  display: 'block',
+                  // textAlign: 'center',
+                  // alignItems: 'center',
+                  height: '50px',
+                  value: '1000',
+                }}
+              />
+            </div>
+          </>
+        ) : (
+          '장바구니가 비어 있습니다.'
+        )}
       </div>
     </div>
   );
